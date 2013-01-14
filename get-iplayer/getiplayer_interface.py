ac33e89c26d481483bf048a9f186abc775b788fb
Andy Gurden <as.gurden@gmail.com>
2013-01-14 03:19:34 +0000

    Searching episode names and descriptions as well as series names. Closes #1.

--- get-iplayer/getiplayer_interface.py
+++ get-iplayer/getiplayer_interface.py
@@ -221,7 +221,7 @@ class GetIPlayer(object):
 		if filter_type == "version":
 			filter_type = "versions"
 		fixed_filtering = self._fix_blank_search(type=type, channel=channel, category=category, version=version)
-		filters = self._call(*([search] if search else []), list=filter_type, **fixed_filtering)
+		filters = self._call(*([search] if search else []), list=filter_type, long="", **fixed_filtering)
 		available_filters = filters.translate(lambda fs: list(parse_listings(fs)))
 		if filter_type == "versions":
 			return available_filters.translate(parse_versions)
@@ -234,12 +234,12 @@ class GetIPlayer(object):
 			return PendingResult(lambda: True, lambda: 0) # Don't have an option to exclude these, but I don't think you can have blank types
 		exclude = {}
 		exclude["exclude-"+blankattrib] = ".+"
-		blank = self._call(*([search] if search else []), type=type, channel=channel, category=category, **exclude)
+		blank = self._call(*([search] if search else []), long="", type=type, channel=channel, category=category, **exclude)
 		return blank.translate(parse_match_count)
 
 	def get_episodes(self, search=None, type="all", channel=".*", category=".*", version=".*"):
 		fixed_filtering = self._fix_blank_search(type=type, channel=channel, category=category, version=version)
-		episodes = self._call(*([search] if search else []), tree="", listformat="<index>: (<episodenum>) <episode>", **fixed_filtering)
+		episodes = self._call(*([search] if search else []), long="", tree="", listformat="<index>: (<episodenum>) <episode>", **fixed_filtering)
 		return episodes.translate(parse_episodes)
 
 	def get_programme_info(self, index, availableversions=None):
